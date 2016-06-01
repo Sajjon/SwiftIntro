399088653cf744438a891ad3b7508306f53fd66c
Alexander Cyon <alex.cyon@gmail.com>
2016-06-01 19:19:07 +0200

    Initial commit, added Swiftlint file

---
+++ SwiftIntro/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SwiftIntro
+//
+//  Created by Alexander Georgii-Hemming Cyon on 01/06/16.
+//  Copyright © 2016 SwiftIntro. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
